6d3622ff533515698ddb84e753ca20657345cc49
Will Spooner <contact@willbspooner.uk>
2026-07-09 20:35:06 +0100

    Update name of settings page

--- src/pkjs/config.js
+++ src/pkjs/config.js
@@ -1,31 +1,31 @@
 module.exports = [
   {
-    type: "heading",
-    defaultValue: "Mainsail Monitor",
+    type: 'heading',
+    defaultValue: 'Klipper Monitor',
   },
   {
-    type: "section",
+    type: 'section',
     items: [
       {
-        type: "heading",
-        defaultValue: "Printer Connection",
+        type: 'heading',
+        defaultValue: 'Printer Connection',
       },
       {
-        type: "input",
-        messageKey: "MoonrakerUrl",
-        defaultValue: "http://printer.lan",
-        label: "Moonraker URL",
+        type: 'input',
+        messageKey: 'MoonrakerUrl',
+        defaultValue: 'http://printer.lan',
+        label: 'Moonraker URL',
         description:
-          "The address of your printer, e.g. http://192.168.1.100 or http://hostname.local",
+          'The address of your printer, e.g. http://192.168.1.100 or http://hostname.local',
         attributes: {
-          placeholder: "http://",
-          type: "url",
+          placeholder: 'http://',
+          type: 'url',
         },
       },
     ],
   },
   {
-    type: "submit",
-    defaultValue: "Save Settings",
+    type: 'submit',
+    defaultValue: 'Save Settings',
   },
 ];
